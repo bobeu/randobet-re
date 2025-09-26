9504bc4624785fdd6189e5536a9f143cbf5f5804
bobeu <bobmatea27@gmail.com>
2025-09-26 12:13:55 +0100

    Added missing dependency

--- fe/src/metadata.ts
+++
@@ -1,6 +0,0 @@
-// import type { Metadata } from 'next';
-
-// export const metadata: Metadata = {
-//     title: 'Randobet - On-Chain Betting Protocol',
-//     description: 'Experience the mystical thrill of on-chain betting with our SpookySwap-themed betting protocol',
-// }
